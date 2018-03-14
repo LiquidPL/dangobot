10ab392bf6e17d76b8961d1632dba71d50fe8593
Krzysztof Gutkowski <krzysio.gutkowski@gmail.com>
2018-03-14 23:52:24 +0100

    Add/remove/edit command

--- plugins/responder/plugin.py
+++ plugins/responder/plugin.py
@@ -2,6 +2,7 @@ from discord.ext import commands
 from discord.ext.commands.view import StringView
 
 from .models import Command
+from db.models import Server
 
 import asyncio
 import functools
@@ -34,6 +35,69 @@ class Responder:
                 await self.bot.send_file(message.channel, command.file.path, content=None)
             else:
                 await self.bot.send_message(message.channel, content=command.response)
+
+    @commands.command(pass_context=True)
+    @commands.has_permissions(manage_messages=True)
+    async def addcommand(self, ctx, trigger : str, response : str):
+        server = Server.objects.get(id=ctx.message.server.id)
+        command = Command(server=server, trigger=trigger, response=response)
+
+        try:
+            command.save()
+
+            await self.bot.send_message(
+                ctx.message.channel,
+                content='Command `{trigger}` added successfully!'.format(trigger=trigger)
+            )
+        except Exception:
+            await self.bot.send_message(
+                ctx.message.channel,
+                content="Couldn't add the command `{trigger}`.".format(trigger=trigger)
+            )
+
+    @commands.command(pass_context=True)
+    @commands.has_permissions(manage_messages=True)
+    async def editcommand(self, ctx, trigger : str, response : str):
+        try:
+            command = Command.objects.get(server=ctx.message.server.id, trigger=trigger)
+        except Command.DoesNotExist:
+            await self.send_message(
+                ctx.message.channel,
+                content='Command `{trigger} not found!`'.format(trigger, trigger)
+            )
+            return
+
+        command.response = response
+
+        try:
+            command.save()
+
+            await self.bot.send_message(
+                ctx.message.channel,
+                content='Command `{trigger}` updated successfully!'.format(trigger=trigger)
+            )
+        except:
+            await self.bot.send_message("Couldn't update command `{trigger}`".format(trigger=trigger))
+
+
+
+    @commands.command(pass_context=True)
+    @commands.has_permissions(manage_messages=True)
+    async def removecommand(self, ctx, trigger : str):
+        command = Command.objects.get(server=ctx.message.server.id, trigger=trigger)
+
+        try:
+            command.delete()
+
+            await self.bot.send_message(
+                ctx.message.channel,
+                'Command `{trigger}` deleted successfully!'.format(trigger=trigger)
+            )
+        except Exception:
+            await self.bot.send_message(
+                ctx.message.channel,
+                "Couldn't delete the command `{trigger}`.".format(trigger=trigger)
+            )
 
 def setup(bot):
     bot.add_cog(Responder(bot))
