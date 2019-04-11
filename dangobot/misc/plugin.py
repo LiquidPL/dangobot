610b396ce5e84050a25a5b24c1d3709d09105feb
Krzysztof Gutkowski <krzysio.gutkowski@gmail.com>
2019-04-12 00:46:00 +0200

    Improved !roll command

--- dangobot/misc/plugin.py
+++ dangobot/misc/plugin.py
@@ -1,37 +1,94 @@
 from discord.ext import commands
 from discord.ext.commands import Cog
 
+from discord import Embed
+
 import re
 import random
+
+DICES = 1
+ROLLS = 2
+FULL_VALUE = 3
 
 
 class Misc(Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.roll_pattern = re.compile(r"([0-9]+)d([0-9]+)")
+        self.dice_pattern = re.compile(r"([0-9]+)d([0-9]+)")
         self.number_pattern = re.compile(r"([0-9]+)")
 
     @commands.command()
     async def roll(self, ctx, *, input: str):
-        ans = 0
-        for x in re.sub(r"\s+", "", input).split('+'):
-            if x:
-                roll = self.number_pattern.fullmatch(x)
+        """
+        Pen-and-paper RPG-style dice roll.
 
-                if roll:
-                    ans += int(roll.group(1))
+        Uses standard dice notation
+        (https://en.wikipedia.org/wiki/Dice_notation).
+        """
+        full_value = 0
+        results = []
+
+        rolls = re.sub(r"\s+", "", input).split("+")
+
+        if len(rolls) > 20:
+            display_format = FULL_VALUE
+        elif len(rolls) > 1:
+            display_format = ROLLS
+        else:  # len(rolls) == 1
+            display_format = DICES
+
+        for roll in rolls:
+            if roll:
+                number_match = self.number_pattern.fullmatch(roll)
+
+                if number_match:
+                    full_value += int(number_match.string)
                     continue
 
-                roll = self.roll_pattern.fullmatch(x)
+                dice_match = self.dice_pattern.fullmatch(roll)
 
-                if not roll:
-                    await ctx.send('Invalid roll!')
+                if not dice_match:
+                    await ctx.send("Invalid roll!")
                     return
 
-                for i in range(int(roll.group(1))):
-                    ans += random.randint(1, int(roll.group(2)))
+                roll_value = 0
+                roll_count = int(dice_match.group(1))
 
-        await ctx.send(ans)
+                if roll_count > 20:
+                    display_format = FULL_VALUE
+
+                for i in range(roll_count):
+                    dice_value = random.randint(1, int(dice_match.group(2)))
+
+                    if display_format == DICES:
+                        results.append(dice_value)
+
+                    roll_value += dice_value
+
+                if display_format == ROLLS:
+                    results.append(roll_value)
+
+                full_value += roll_value
+
+        embed = Embed(title=f"Rolling {input}")
+
+        if not display_format == FULL_VALUE:
+            for i, result in enumerate(results, start=1):
+                embed.add_field(
+                    name=self.roll_string(display_format).format(i),
+                    value=result,
+                    inline=True,
+                )
+
+        embed.add_field(name="**Full value**", value=full_value, inline=False)
+
+        await ctx.send(embed=embed)
+
+    def roll_string(self, format):
+        if format == DICES:
+            return "Dice {}"
+        else:
+            return "Roll {}"
 
 
 def setup(bot):
