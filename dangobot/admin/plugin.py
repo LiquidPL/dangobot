cf58bec62255f18d1f9eec727fce421722367b70
Krzysztof Gutkowski <krzysio.gutkowski@gmail.com>
2019-10-03 22:36:52 +0200

    Add an admin command to send a message to channel

--- dangobot/admin/plugin.py
+++ dangobot/admin/plugin.py
@@ -1,6 +1,12 @@
-from discord.ext.commands import Cog, is_owner
+from discord.ext.commands import (
+    Cog,
+    is_owner,
+    NoPrivateMessage,
+    BadArgument,
+    MissingPermissions,
+)
 from discord.ext import commands
-
+from discord import TextChannel, DMChannel
 from dangobot.core.helpers import guild_fetch_or_create
 
 
@@ -17,6 +23,32 @@ class Admin(Cog):
     async def resetguild(self, ctx):
         await guild_fetch_or_create(self.bot.db_pool, ctx.guild)
         await ctx.send(content=f"Reset guild {ctx.guild.name}")
+
+    @commands.command(hidden=True, rest_as_raw=False)
+    async def say(self, ctx, channel: TextChannel, *, message):
+        if isinstance(ctx.channel, DMChannel) and not self.bot.is_owner(
+            ctx.author
+        ):
+            raise NoPrivateMessage(
+                message="You can't use this command in a DM!"
+            )
+
+        if ctx.guild.id != channel.guild.id:
+            raise BadArgument(
+                message="You can't send messages outside of your server!"
+            )
+
+        if not ctx.channel.permissions_for(ctx.author).administrator:
+            raise MissingPermissions(["administrator"])
+
+        await channel.send(content=message)
+
+    @say.error
+    async def say_errors(self, ctx, error):
+        if isinstance(error, NoPrivateMessage) or isinstance(
+            error, BadArgument
+        ):
+            await ctx.send(content=error)
 
 
 def setup(bot):
