70afde6e3b34624fcd239104083a4a326a11d1b6
Krzysztof Gutkowski <krzysio.gutkowski@gmail.com>
2021-02-08 22:31:03 +0100

    Nuke the remains of travis/docker hub

--- docker_push.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-docker tag dangobot "liquidpl/dangobot:latest"
-docker tag dangobot "liquidpl/dangobot:$(git log -1 --pretty=%h)"
-docker push liquidpl/dangobot
